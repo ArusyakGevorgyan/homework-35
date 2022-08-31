a772ce281f058bb1ccafcad5dea5bb7ec59281fc
ArusyakGevorgyan <arusikgevorgyan95@gmail.com>
2022-08-31 11:20:29 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date('1 1 2023 00:00:00');
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerText = d;
+    hours.innerText = h;
+    minutes.innerText = m;
+    seconds.innerText = s;
+
+}
+
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
